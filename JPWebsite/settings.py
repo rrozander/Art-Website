e715c3b23b1490d5002bbf49ccd2b09dd4d7e4cc
Riley Rozander <rileyrozander@gmail.com>
2022-02-24 16:07:17 -0800

    setup static files to load globally or within app

--- JPWebsite/settings.py
+++ JPWebsite/settings.py
@@ -126,14 +126,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
 STATICFILES_DIRS = [
-  os.path.join(BASE_DIR, 'static'),
+  BASE_DIR / 'static',
 ]
 
 # Default primary key field type
--- art/views.py
+++ art/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import get_object_or_404, render
 
 
 # Create your views here.
+
+def art_view(request):
