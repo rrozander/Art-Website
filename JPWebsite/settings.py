b852bc3d06c6e41e6a0a2b7c0f268d90a7eba8ce
Riley Rozander <rileyrozander@gmail.com>
2022-03-21 14:18:26 -0700

    Added .gitignore

--- JPWebsite/settings.py
+++ JPWebsite/settings.py
@@ -21,10 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#!&f64430*d#xa^8ge1j(%nlj8am%yw(r73@lzu5s!sbqhs$u+'
+# SECRET_KEY = 'django-insecure-#!&f64430*d#xa^8ge1j(%nlj8am%yw(r73@lzu5s!sbqhs$u+'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 
+'django-insecure-#!&f64430*d#xa^8ge1j(%nlj8am%yw(r73@lzu5s!sbqhs$u+')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+
 
 ALLOWED_HOSTS = []
 
