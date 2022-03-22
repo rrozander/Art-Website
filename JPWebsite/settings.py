de230d4d7dd5528cf790cd149b19d2645fe6a5ea
Riley Rozander <rileyrozander@gmail.com>
2022-03-21 23:47:59 -0700

    changed allowed hosts

--- JPWebsite/settings.py
+++ JPWebsite/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 
-ALLOWED_HOSTS = ['intense-shore-78196.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['julia-pearson-art.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
