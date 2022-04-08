5adeb4d400dcaa6132249ed1e7de6af32172fd6e
Riley Rozander <rileyrozander@gmail.com>
2022-04-07 22:01:25 -0700

    changed for production

--- JPWebsite/media_storage.py
+++ JPWebsite/media_storage.py
@@ -2,4 +2,4 @@ import os
 from storages.backends.s3boto3 import S3Boto3Storage
 
 class MediaStorage(S3Boto3Storage):
-  bucket_name = os.environ.get('S3_BUCKET_NAME', 'jp-art-website')
+  bucket_name = os.environ.get('S3_BUCKET_NAME')
